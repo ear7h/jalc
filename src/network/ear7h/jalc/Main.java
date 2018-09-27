53dd9c5949163e2d8ee7c4dac51fd852561a2ec6
ear7h <julio.grillo98@gmail.com>
2018-09-26 21:22:21 -0700

    got a simple lexer working

--- src/network/ear7h/jalc/Main.java
+++ src/network/ear7h/jalc/Main.java
@@ -10,19 +10,19 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("welcome to jalc 0.0.0");
 		Scanner kbd = new Scanner(System.in);
+		Lexer lex = new Lexer();
 
-		for (String[] exp = kbd.nextLine().split(" ");
-			exp[0] != "exit";
-			exp = kbd.nextLine().split(" ")) {
-
+		for (String exp = kbd.nextLine();
+			!exp.equals("exit");
+			exp = kbd.nextLine()) {
 			try {
-				double val = (new Expression(exp)).eval();
-				System.out.println(val);
+				lex.add(exp + "\n");
 			} catch (Exception e) {
 				e.printStackTrace();
-				System.out.println(e);
 			}
+			System.out.println(lex.toString());
 		}
+		System.out.println("exiting");
 	}
 }
 
@@ -95,27 +95,14 @@ class Expression {
 
 class Token {
 	public enum Type {
-		SYMBOL, NUMBER, STRING, NEWLINE;
-
-		// String toString() {
-		// 	switch (this) {
-		// 		case SYMBOL:
-		// 			return "symbol";
-		// 		case NUMBER:
-		// 			return "number";
-		// 		case STRING:
-		// 			return "string";
-		// 		case NEWLINE:
-		// 			return "newline";
-		// 	}
-		// }
+		IDENT, NUMBER, STRING, SPACE, NEWLINE, BACKSLASH;
 	}
 
-	Type type;
-	int line;
-	int col;
+	public final Type type;
+	public final int line;
+	public final int col;
 
-	String literal;
+	public final String literal;
 
 	Token(Type t, int line, int col, String literal) {
 		this.type = t;
@@ -125,7 +112,59 @@ class Token {
 	}
 
 	public String toString() {
-		return String.format("%d:%d %s %s", this.line, this.col, this.type.toString(), this.literal);
+		return String.format("%d:%d %s %s;", this.line, this.col, this.type.toString(), this.literal);
+	}
+}
+
+class SourceScanner{
+	String data;
+	int cursor = -1;
+	int line = 0;
+	int col = 0;
+	boolean hadNewline = false;
+
+	public SourceScanner(String src) {
+		this.data = src;
+	}
+
+	public int getLn() {
+		return this.line;
+	}
+
+	public int getCol() {
+		return this.col;
+	}
+
+	public boolean scan() {
+		this.cursor++;
+		if (cursor >= this.data.length()) {
+			return false;
+		}
+		char c = this.data.charAt(this.cursor);
+		if (hadNewline) {
+			this.line++;
+			this.col = 0;
+		} else {
+			this.col++;
+		}
+		this.hadNewline = c == '\n';
+		return true;
+	}
+
+	public char current() {
+		return this.data.charAt(this.cursor);
+	}
+
+	public char next() {
+		int ni = this.cursor + 1;
+		if (ni >= this.data.length()) {
+			return '\0';
+		}
+		return this.data.charAt(ni);
+	}
+
+	public boolean hasNext() {
+		return this.cursor < this.data.length() - 1;
 	}
 }
 
@@ -136,28 +175,54 @@ class Lexer {
 		this.tokens = new LinkedList<>();
 	}
 
-	public void input(String src) throws Exception {
-		String[] arr = src.split(" ");
-		for (int i = 0; i < arr.length; i++) {
+	public void add(String src) throws Exception {
+		System.out.println(src + ";");
+		System.out.println(src.charAt(src.length() - 1) == '\n');
+		SourceScanner scanner = new SourceScanner(src);
 
-			if (arr[i].length() == 0) {
-				continue;
-			} else if (arr[i] == "\n") {
-				this.tokens.add(new Token(Token.Type.NEWLINE, 0, 0, arr[i]));
-			} else if (arr[i] == "\\") {
-				if (i < arr.length - 1 && arr[i+1] == "\n") {
-					i++;
+		while (scanner.scan()){
+			//curChar = scanner.next()) {
+			System.out.println("char: " + scanner.current());
+
+			// whitespace
+			if (scanner.current() == ' ') {
+				this.tokens.add(new Token(Token.Type.SPACE, scanner.getLn(), scanner.getCol(), " "));
+			// newline
+			} else if (scanner.current() == '\n') {
+				this.tokens.add(new Token(Token.Type.NEWLINE, scanner.getLn(), scanner.getCol(), "\n"));
+			// backslash
+			} else if (scanner.current() == '\\') {
+				this.tokens.add(new Token(Token.Type.BACKSLASH, scanner.getLn(), scanner.getCol(), "\\"));
+			// numeric literal
+			} else if (Character.isDigit(scanner.current())){
+				String lit = "" + scanner.current();
+				while (scanner.hasNext() && (
+					Character.isDigit(scanner.next()) || 
+					Character.isLetter(scanner.next()))) {
+					scanner.scan();
+					scanner.current();
 				}
-				continue;
-			} else if (Character.isLetter(arr[i].charAt(0))) {
-				this.tokens.add(new Token(Token.Type.SYMBOL, 0, 0, arr[i]));
-			} else if (Character.isDigit(arr[i].charAt(0))) {
-				this.tokens.add(new Token(Token.Type.NUMBER, 0, 0, arr[i]));
-			} else{
-				throw new Exception("unrecognized token: " + arr[i]);
-			}
 
+				this.tokens.add(new Token(Token.Type.NUMBER, scanner.getLn(), scanner.getCol(), lit));
+			// identifier
+			} else if (Character.isLetter(scanner.current())) {
+				String lit = "" + scanner.current();
+				while (scanner.hasNext() && (
+					Character.isDigit(scanner.next()) || 
+					Character.isLetter(scanner.next()))) {
+					scanner.scan();
+					lit += scanner.current();
+				}
+
+				this.tokens.add(new Token(Token.Type.IDENT, scanner.getLn(), scanner.getCol(), lit));
+			} else {
+				throw new Exception("unrecognized token: " + scanner.current());
+			}
 		}
+	}
+
+	public boolean hasToken() {
+		return !this.tokens.empty();
 	}
 
 	public Token getToken() {
@@ -170,18 +235,76 @@ class Lexer {
 			ret += tk.toString() + "\n";
 		}
 
-		return ret;
+		return ret.substring(0, ret.length() - 1);
 	}
 }
 
-interface Type {
-	String name();
+class Proc {
+	// a procedure is a list of statements
+	Queue<Stmt> stmts;
+
+	public void exec() {
+		for (var stmt : this.stmts) {
+			stmt.exec();
+		}
+	}
+}
+
+class Prog extends Proc {
+	definition
+}
+
+abstract class Stmt {
+	public enum Type {
+		EXPR, // simplifies to single value `add 1 1` or `1 + 1`
+		ASSIGN, // `{IDENT} = EXPR` or 
+		DEC // `var {IDENT} {IDENT}` where the second IDENT refers to a type. Or `var {IDENT} = EXPR` ;
+	}
+	abstract public void exec();
+}
+
+class Parser {
+	Lexer lex;
+
+	public Parser(Lexer lex) {
+		this.lex = lex;
+	}
+
+	public String parse() {
+		Token curToken;
+		while (this.lex.hasToken()) {
+			// read entire statement
+			Queue<Token> stmt = new LinkedList<>();
+
+			while(this.lex.hasToken()) {
+				Token tk = this.lex.getToken();
+				if ()
+			}
+
+			switch (curToken.type) {
+			}
+
+		}
+	}
+}
+
+abstract class Expression {
+	Lexer lex;
+	public Expression(Lexer lex) {
+		this.lex = lex;
+	}
+
+	abstract public Value eval();
 }
 
 interface Value {
-	void parse(Lexer l);
-	String type();
-	String toString();
+	//public String kind();
+	public String type();
+	public String toString();
+}
+
+class Heap {
+	Map<String, Expression> symbols;
 }
 
 abstract class Function {
